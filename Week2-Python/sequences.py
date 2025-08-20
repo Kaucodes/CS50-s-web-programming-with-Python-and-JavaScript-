02a910def10d7444dba68296ea9800fda9c2923f
Kaucodes <kautharahamd970@gmail.com>
2025-08-20 19:18:24 +0300

    Python Week

---
+++ Week2-Python/sequences.py
@@ -0,0 +1,10 @@
+# name = "Harry"
+# print(name[0])
+
+# names = ["Harry", "Ron", "Hermione"]
+# print(names[2])
+
+# List - sequence of mutable values
+# Tuple - sequence of immutable values
+# Set - collection of unique values
+# Dictionary - collection of key-value pairs
